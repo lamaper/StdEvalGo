2c2209ef45ff73ffd2af2231aede5bea1e59de8a
lamaper <lamaper@qq.com>
2025-07-24 10:31:16 +0800

    first commit

---
+++ SEG_backend/src/main/java/org/birdt/stdevalgo/SegBackendApplication.java
@@ -0,0 +1,13 @@
+package org.birdt.stdevalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SegBackendApplication.class, args);
+    }
+
+}
